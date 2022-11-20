1283b72799c38dd7127e06cfe9e9612642f42202
Minisiia <ar_tik@yahoo.com>
2022-11-20 15:47:55 +0200

    Java Start. 2. Machine mathematics. Data types. Logical variables

---
+++ src/BooleanDataType.java
@@ -0,0 +1,6 @@
+public class BooleanDataType {
+    public static void main(String[] args) {
+        boolean grassIsGreen = true;
+        System.out.println("Трава зелена " + grassIsGreen);
+    }
+}
